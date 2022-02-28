7c5a07c9b05f659444c6a93b474cc5e7b0d74e41
Czekras <tinio.johnchristian@gmail.com>
2022-02-28 11:01:49 +0900

    Deploy: Github Pages

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  const scrollpos = localStorage.getItem('scrollpos');
+  if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function(e) {
+  localStorage.setItem('scrollpos', window.scrollY);
+};
+
+const fadingElement = document.querySelectorAll('.fade-in');
+const fadeOption = {
+  rootMargin: " 0px 0px -500px 0px"
+};
+
+const fadeOnScroll = new IntersectionObserver(function(entries, apeparOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      console.log(`enter ${entry.target}`);
+      entry.target.classList.add('appear');
+      apeparOnScroll.unobserve(entry.target);
+    }
+  })
+}, fadeOption);
+
+fadingElement.forEach(fade => {
+  fadeOnScroll.observe(fade);
+});
